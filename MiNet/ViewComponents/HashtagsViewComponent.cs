3d03c6189d06b4b2abcd0ff1bc5c2d39640781a9
Tuan <tranhunganhtuan2409@gmail.com>
2026-03-17 11:15:05 +0700

    Update ViewComponent of Hashtag with real data

--- MiNet/ViewComponents/HashtagsViewComponent.cs
+++ MiNet/ViewComponents/HashtagsViewComponent.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MiNet.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace MiNet.ViewComponents
 {
     public class HashtagsViewComponent : ViewComponent
     {
+        private readonly AppDbContext _context;
+
+        public HashtagsViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+        
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+            var OneWeekAgoNow = DateTime.Now.AddDays(-7);
+
+            var top3Hashtags = await _context.Hashtags
+                .Where(h => h.DateCreated >= OneWeekAgoNow)
+                .OrderByDescending(n => n.DateCreated)
+                .Take(3)
+                .ToListAsync();
+
+            return View(top3Hashtags);
         }
     }
 }
