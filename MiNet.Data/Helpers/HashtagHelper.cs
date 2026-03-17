a768d6507a4780994de28041d843d51bd31f49df
Tuan <tranhunganhtuan2409@gmail.com>
2026-03-17 10:55:57 +0700

    Added hashtag Helper

---
+++ MiNet.Data/Helpers/HashtagHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MiNet.Data.Helpers
+{
+    public static class HashtagHelper
+    {
+        public static List<string> GetHashtags(string postText)
+        {
+            var hashtagPattern = new Regex(@"#\w+");
+            var matches = hashtagPattern.Matches(postText)
+                .Select(match => match.Value.TrimEnd('.',',','!','?'))
+                .Distinct()
+                .ToList();
+
+            return matches;
+        }
+    }
+}
