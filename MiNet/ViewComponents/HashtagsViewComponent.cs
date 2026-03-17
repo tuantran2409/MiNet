c0c543ba16c68d466d7c5a858596c12b18bdbe99
Tuan <tranhunganhtuan2409@gmail.com>
2026-03-17 10:50:15 +0700

    Convert PartialView to ViewComponent

---
+++ MiNet/ViewComponents/HashtagsViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiNet.ViewComponents
+{
+    public class HashtagsViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
