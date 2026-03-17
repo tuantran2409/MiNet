dadb33ff2fd810e6a813f0125eef0caa5ebe4fc5
Tuan <tranhunganhtuan2409@gmail.com>
2026-03-17 10:41:36 +0700

    Add Hashtag Table

--- MiNet.Data/AppDbContext.cs
+++ MiNet.Data/AppDbContext.cs
@@ -17,6 +17,7 @@ namespace MiNet.Data
         public DbSet<Favorite> Favorites { get; set; }
         public DbSet<Report> Reports { get; set; }
         public DbSet<Story> Stories { get; set; }
+        public DbSet<Hashtag> Hashtags { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
