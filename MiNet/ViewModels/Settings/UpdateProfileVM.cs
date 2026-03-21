3f5521ec53fb30c6adc9cb642cdd8b8116d9fc33
Tuan <tranhunganhtuan2409@gmail.com>
2026-03-21 19:51:47 +0700

    Add user setting pages

---
+++ MiNet/ViewModels/Settings/UpdateProfileVM.cs
@@ -0,0 +1,10 @@
+﻿namespace MiNet.ViewModels.Settings
+{
+    public class UpdateProfileVM
+    {
+        public string FullName { get; set; }
+        public string EmailAddress { get; set; }
+        public string UserNamer { get; set; }
+        public string Bio { get; set; }
+    }
+}
