b8be0bdbdd1a259f91c90b0edaba6c47609ea39e
Tuan <tranhunganhtuan2409@gmail.com>
2026-03-29 20:30:19 +0700

    add admin init file and appsetting development template

---
+++ MiNet/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using MiNet.Data.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiNet.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IAdminService _adminService;
+        public AdminController(IAdminService adminService)
+        {
+            _adminService = adminService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
